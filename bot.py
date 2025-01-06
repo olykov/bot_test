16cca51ce8166da6affbe5861e7f258554cd3524
Oleksii Lykov <oleksiilykov@MacBook-Pro.local>
2025-01-06 13:29:09 +0400

    polling fixed

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import F, Bot, Dispatcher, Router, types
 from aiogram.filters import CommandStart
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
 from fastapi import FastAPI, Request
@@ -18,24 +18,25 @@ async def webhook(request: Request):
         return JSONResponse(content={"status": 500, "message": str(e)}, status_code=500)
 
 async def start_polling():
-    bot = Bot(token=os.environ['TG_TOKEN'])
+    bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
+    router = Router()
+    dp.include_router(router)
     dp.callback_query.middleware(CallbackAnswerMiddleware(pre=True, text="🤔"))
 
     def is_api_group(chat_id):
         return chat_id == os.environ['TG_ADMIN']
 
-    @dp.message_handler(CommandStart())
+    @router.message(CommandStart())
     async def start(message: types.Message):
         await message.answer('hi!')
 
-    await dp.start_polling(bot)
-
-    @bot.message_handler(content_types=["text"])
-    def foo(message):
+    @router.message()
+    async def foo(message: types.Message):
         if is_api_group(str(message.chat.id)):
-            bot.send_message(message.chat.id, 'ha-ha')
+            await message.answer('ha-ha')
 
+    await dp.start_polling(bot)
     
 
 async def run_fastapi():
@@ -49,6 +50,9 @@ async def main():
 # for some reasons in this specific task we need poll while we have active endpoint on port
 if __name__ == "__main__":
     try:
+        BOT_TOKEN = os.getenv("TG_TOKEN")
+        if not BOT_TOKEN or not os.getenv("TG_ADMIN"):
+            raise ValueError("Bot token or TG_ADMIN aren't set. Please configure your environment.")
         asyncio.run(main())
     except KeyboardInterrupt:
         print("Stopped manually.")
